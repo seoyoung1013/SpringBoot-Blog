f417b0222a26624a26282009ede96e5597071bde
seoyoung1013 <lemi01@naver.com>
2023-10-23 23:53:08 +0900

    Exception 처리

---
+++ src/main/java/com/cos/blog/handler/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.cos.blog.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+//import com.cos.blog.dto.ResponseDto;
+
+@ControllerAdvice
+@RestController
+public class GlobalExceptionHandler {
+	
+	@ExceptionHandler(value=Exception.class)
+	public String handleArgumentException(Exception e) {
+		return "<h1>"+e.getMessage()+"</h1>";
+	}
+	
+	/*@ExceptionHandler(value=Exception.class)
+	public ResponseDto<String> handleArgumentException(Exception e) {
+		return new ResponseDto<String>(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage()); // 500
+	}*/
+}
