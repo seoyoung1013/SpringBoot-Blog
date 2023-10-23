f6dcd2f015406a29a5408cc7d0763c817619b234
seoyoung1013 <lemi01@naver.com>
2023-10-23 22:17:21 +0900

    회원 insert, Role컬럼 enum 사용

--- src/main/java/com/cos/blog/model/User.java
+++ src/main/java/com/cos/blog/model/User.java
@@ -2,11 +2,12 @@ package com.cos.blog.model;
 
 import java.sql.Timestamp;
 
-import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -23,6 +24,7 @@ import lombok.NoArgsConstructor;
 @Builder //빌더 패턴
 //ORM -> Java(다른언어) Object -> 테이블로 매핑해주는 기술
 @Entity //User 클래스가 MySQL에 테이블이 생성이 된다.
+//@DynamicInsert //Insert시에 null 필드를 제외시켜준다.
 public class User {
 
 	@Id //Primary key
@@ -38,10 +40,12 @@ public class User {
 	@Column(nullable = false, length = 50) 
 	private String email;
 	
-	@ColumnDefault("'user'")
-	private String role; //Enum을 쓰는게 좋다. 추후에 변경 예정 //admin, user, manager 도메인(범위) 설정가능
+	//@ColumnDefault("user")
+	//DB는 RoleType이라는게 없다.
+	@Enumerated(EnumType.STRING)
+	private RoleType role; //Enum을 쓰는게 좋다. 추후에 변경 예정 //ADMIN, USER, 도메인(범위) 설정가능
 	
-	@CreationTimestamp //시간이 자동 입력
+	@CreationTimestamp //현재 시간이 자동 입력
 	private Timestamp createDate;
 
 }
