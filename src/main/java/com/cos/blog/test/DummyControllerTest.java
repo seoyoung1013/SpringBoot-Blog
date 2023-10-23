7008778de7c505c58cef4f2d3f1e8098115f51ef
seoyoung1013 <lemi01@naver.com>
2023-10-23 23:13:14 +0900

    user id 전체 select & paging

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -1,8 +1,13 @@
 package com.cos.blog.test;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,11 +22,22 @@ import com.cos.blog.repository.UserRepository;
 public class DummyControllerTest {
 
 	
-	
 	@Autowired //의존성 주입(DI)
 	private UserRepository userRepository;
 	
+	@GetMapping("/dummy/users")
+	public List<User> list(){
+		return userRepository.findAll();
+	}
 	
+	//한페이지당 2건에 데이터를 리턴받아 볼 예쩡
+	@GetMapping("/dummy/user")
+	public List<User> pageList(@PageableDefault(size=2,sort="id",direction=Sort.Direction.DESC) Pageable pageable){
+		Page<User> pagingUser = userRepository.findAll(pageable);
+		List<User> users = pagingUser.getContent();
+		return users;
+	}
+
 	//{id}주소로 파라미터를 전달받을 수 있음.
 	//http://localhost:8181/blog/dummy/user/3
 	@GetMapping("dummy/user/{id}")
