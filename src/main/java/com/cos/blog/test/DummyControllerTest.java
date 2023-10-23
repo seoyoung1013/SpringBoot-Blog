b0ecd9c266d94182da5de523e0d7fa9b58e4f56c
seoyoung1013 <lemi01@naver.com>
2023-10-23 22:55:00 +0900

    회원id select

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -1,6 +1,10 @@
 package com.cos.blog.test;
 
+import java.util.function.Supplier;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +12,7 @@ import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 
+//html  파일이 아니라 data를 리턴해주는 controller = RestController
 @RestController
 public class DummyControllerTest {
 
@@ -15,6 +20,30 @@ public class DummyControllerTest {
 	
 	@Autowired //의존성 주입(DI)
 	private UserRepository userRepository;
+	
+	
+	//{id}주소로 파라미터를 전달받을 수 있음.
+	//http://localhost:8181/blog/dummy/user/3
+	@GetMapping("dummy/user/{id}")
+	public User detail(@PathVariable int id) {
+		//user/4을 찾으면 내가 데이터베이스에서 못찾아오게 되면 user가 null이 될 것 아냐?
+		//그럼 return null이 리턴이 되자나. 그럼 프로그램에 문제가 있지 않겠지?
+		//Optional로 너의 user 객체를 감싸서 가져올테니 null인지 아닌지 판단해서 return 해.
+		User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
+			@Override
+			public IllegalArgumentException get() {
+				return new IllegalArgumentException("해당 사용자가 없습니다.");
+			}
+		});
+		//요청 : 웹 브라우저
+		// user 객체 = 자바 오브젝트
+		// 변환 (웹 브라우저가 이해할 수 있는 데이터 ) -> json(Gson 라이브러리)
+		// 스프링부트 = MessageConverter라는 애가 응답시에 자동 작동
+		//만약에 자바 오브젝트를 리턴하게 되면 MessageConverter가 Jackson라이브러리를 호출해서
+		// user 오브젝트를 json으로 변환해서 브라우저에게 던져줍니다.
+		return user;
+		
+	}
 	
 	//http://localhost:8181/blog/ummy/join(요청)
 	//http의 body에 username, password, email 데이터를 가지고 (요청)
