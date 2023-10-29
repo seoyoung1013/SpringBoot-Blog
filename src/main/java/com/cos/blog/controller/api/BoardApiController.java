9eeda855736028b4a77c896c279537656a62b178
seoyoung1013 <lemi01@naver.com>
2023-10-29 22:36:34 +0900

    sysout 삭제

--- src/main/java/com/cos/blog/controller/api/BoardApiController.java
+++ src/main/java/com/cos/blog/controller/api/BoardApiController.java
@@ -37,10 +37,6 @@ public class BoardApiController {
 	
 	@PutMapping("/api/board/{id}")
 	public ResponseDto<Integer> update(@PathVariable int id, @RequestBody Board board){
-		System.out.println("BoardApiController : update : id : "+id);
-		System.out.println("BoardApiController : update : board : "+board.getTitle());
-		System.out.println("BoardApiController : update : board : "+board.getContent());
-		System.out.println("BoardApiController : update : board : "+board.getTravelWith());
 		boardService.글수정하기(id, board);
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
